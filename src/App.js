d211392e04f16b66ff53fd2532aa2a8abdf554ed
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2020-03-26 08:58:01 -0600

    login styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
+import Login from "./Login";
 
 const my404 = () => {
   return <div>You're lost, page not found</div>;
