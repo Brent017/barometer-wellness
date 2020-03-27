fc39a86efac1945b6a0c01de95028defb6177e6c
Brent Ottke <marieottke@Maries-MacBook-Air.local>
2020-03-27 10:04:12 -0600

    profile page

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -1,14 +1,33 @@
 import React, { Component } from "react";
+import { Grid } from "semantic-ui-react";
 
 class Profile extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    this.state = {
+      username: "",
+      zipCode: "",
+      history: []
+    };
   }
 
+  getWeather = async () => {};
+
+  getHistory = async () => {};
+
   render() {
-    return <div>Profile page</div>;
+    return (
+      <Grid stackable columns={3} padded className="Profile">
+        <Grid.Row>
+          <Grid.Column width={1}></Grid.Column>
+          <Grid.Column width={14}>
+            <h2>Welcome USER!</h2>
+          </Grid.Column>
+          <Grid.Column width={1}></Grid.Column>
+        </Grid.Row>
+      </Grid>
+    );
   }
 }
 
